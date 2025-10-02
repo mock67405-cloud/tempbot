2689d649115af2d3e8d6348efc49bd8791919a45
mock67405-cloud <mock67405@gmail.com>
2025-10-03 00:12:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     AUTH_USER = os.environ.get('8456187383', '').split(',')
-    AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+    AUTH_USERS = [int(8456187383) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = "Crystal™"#Here You Can Change with Your Name  or any custom name or title you prefer
